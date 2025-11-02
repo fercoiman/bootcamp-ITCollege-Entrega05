e1149902e0612ed3bf7056ac16d1ebf29a579df0
fercoiman <fcoiman@frba.utn.edu.ar>
2025-11-01 21:12:41 -0300

    cambio a dos componentes en lugar de 3

--- components/Contenido.jsx
+++ components/Contenido.jsx
@@ -7,14 +7,41 @@ que permita cambiar el fondo de dicho texto entre los colores amarillo,
 cyan y naranja de
 manera rotativa */
 
+import { useState } from "react";
+import Boton from "./Boton";
+
 function Contenido(props) {
+  const [color, setColor] = useState("yellow");
+  const [index, setIndex] = useState(0);
+  const vectorColores = ["yellow", "orange", "cyan"];
+
+  function changeTextColor() {
+    const nextIndex = (index + 1) % vectorColores.length;
+    setIndex(nextIndex);
+    setColor(vectorColores[index]);
+  }
+
   const paragraphStyle = {
-    color: "white",
+    color: color,
     backgroundColor: props.color,
     fontSize: "60px",
   };
 
-  return <p style={paragraphStyle}>{props.texto}</p>;
+  const buttonStyle = {
+    color: "white",
+    backgroundColor: "black",
+    fontSize: "30px",
+  };
+
+  return (
+    <>
+      <p style={paragraphStyle}>{props.texto}</p>
+      <br />
+      <button onClick={changeTextColor} style={buttonStyle}>
+        {props.texto}
+      </button>
+    </>
+  );
 }
 
 export default Contenido;
--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,8 @@ import "./App.css";
 import Encabezado from "../components/Encabezado";
 import Contenido from "../components/Contenido";
 import Boton from "../components/Boton";
-import { useState } from "react";
 
 function App() {
-  const [colorFondo, setColorFondo] = useState("Yellow");
-
-  function cambiarColor(color) {
-    setColorFondo(color);
-  }
-
   const estiloEncabezado = { color: "red" };
   const textoEncabezado = "Este componente es STATELESS.";
 
@@ -22,7 +15,6 @@ function App() {
       <br />
       <Contenido texto={contenidoTexto} />
       <br />
-      <Boton texto="Cambiar Color" />
     </>
   );
 }
